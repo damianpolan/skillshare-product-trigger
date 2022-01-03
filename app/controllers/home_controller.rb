225a213cfb0a2359dc824ea36b1e612e6addde74
Damian Polan <damianpolan@Damians-MacBook-Pro.local>
2022-01-03 12:49:22 -0500

    Chart premium products

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,5 +8,15 @@ class HomeController < ApplicationController
   def index
     @shop_origin = current_shopify_domain
     @host = params[:host]
+
+    session = Shop.retrieve_by_shopify_domain(@shop_origin)
+    ShopifyAPI::Session.with_session(session) do
+      all_products = ShopifyAPI::Product.find(:all)
+
+      @premium_count = all_products.count do |product|
+        product.tags.split(',').include?(ProductsCreateJob::PREMIUM_PRODUCT_TAG)
+      end
+      @not_premium_count = all_products.count - @premium_count
+    end
   end
 end
