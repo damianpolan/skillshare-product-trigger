bd7ec08a4b79f19c1b1e9b566bb256775d7e36e5
Damian Polan <damianpolan@Damians-MacBook-Pro.local>
2022-01-03 11:59:43 -0500

    Set premium product tag when over static premium price

--- app/jobs/products_create_job.rb
+++ app/jobs/products_create_job.rb
@@ -1,7 +1,31 @@
 class ProductsCreateJob < ActiveJob::Base
-  def perform(params)
+  PREMIUM_PRODUCT_MINUMUM_PRICE = 50.00
+  PREMIUM_PRODUCT_TAG = 'premium'
 
-    puts "PRODUCTS CREATE JOB"
+  def perform(params)
+    session = Shop.retrieve_by_shopify_domain(params[:shop_domain])
+    ShopifyAPI::Session.with_session(session) do
+      product_id = params.dig(:webhook, :id)
+      product = ShopifyAPI::Product.find(product_id)
+
+      add_premium_tag_maybe(product)
+    end
+  end
+
+  private
+
+  def add_premium_tag_maybe(product)
+    product.variants.each do |variant|
+      if variant.price.to_d >= PREMIUM_PRODUCT_MINUMUM_PRICE
+        add_tag(product, PREMIUM_PRODUCT_TAG)
+        return product.save
+      end
+    end
+  end
+
+  def add_tag(product, tag)
+    # shopify will automatically de-dup tags, and handle extra comma at beginning of string.
+    product.tags += ",#{tag}"
   end
 end
 
